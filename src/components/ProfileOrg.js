36e2cecbb5a4fd9ab30993e3fc4615446dc65069
KarinaMontiel <karina.e.montiel.24@dartmouth.edu>
2021-05-22 18:46:51 -0400

    Pushing user info rendering

---
+++ src/components/ProfileOrg.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { renderUserInfo } from '../actions';
+
+class ProfileOrg extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    this.props.renderUserInfo(this.props.match.params.postID);
+  }
+
+  render() {
+    return (
+      <div>Org name:{this.props.org.orgname} </div>
+    );
+  }
+}
+function mapStateToProps(reduxState) {
+  return {
+    org: reduxState.org,
+  };
+}
+
+export default connect(mapStateToProps, { renderUserInfo })(ProfileOrg);
