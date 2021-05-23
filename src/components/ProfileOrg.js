7929643e3ced97f50a7dcf084612ee419ede87f2
willperez23 <william.perez.23@dartmouth.edu>
2021-05-23 16:53:22 -0400

    profile styling changes

--- src/components/ProfileOrg.js
+++ src/components/ProfileOrg.js
@@ -14,9 +14,24 @@ const ProfileOrg = (props) => {
 
   return (
     <div>
-      <h2>Person of contact name : {org.pocname}</h2>
-      <h3>Email : {org.email} </h3>
-      <h3>Location : {org.location}</h3>
+      <div className="leftBar">
+        <h1 className="title">Welcome! {org.name}</h1>
+        <div className="update">
+          <h3 className="boldtwentyfour">Person of contact name : </h3>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Edit_icon_%28the_Noun_Project_30184%29.svg/1024px-Edit_icon_%28the_Noun_Project_30184%29.svg.png"
+            alt="edit button"
+            width="25px"
+          />
+        </div>
+        {/* <h3 className="boldtwentyfour">Location :</h3> */}
+        {/* <h3 className="sixteenpoint">{org.events}</h3> */}
+        <h3 className="sixteenpoint">{org.pocname}</h3>
+        <h3 className="sixteenpoint">{org.location}</h3>
+        <h3 className="boldtwentyfour">Email:</h3>
+        <h3 className="sixteenpoint"> {org.email} </h3>
+        <button type="button">Sign Out </button>
+
+      </div>
 
     </div>
   );
