72acdc3f4312d3c537c8d8cd150a4a08eafae4ef
KarinaMontiel <karina.e.montiel.24@dartmouth.edu>
2021-05-30 16:19:09 -0400

    network mentor page static done

--- src/components/Network/NetworkMentor.js
+++ src/components/Network/NetworkMentor.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-// import Card from '@material-ui/core/Card';
-// import CardActions from '@material-ui/core/CardActions';
-// import CardContent from '@material-ui/core/CardContent';
-// import Button from '@material-ui/core/Button';
-// import Typography from '@material-ui/core/Typography';
 
 class NetworkMentor extends Component {
   constructor(props) {
@@ -18,14 +13,26 @@ class NetworkMentor extends Component {
     return (
       <div>
         <div id="banner">Network</div>
+        <div className="searchBar input-group rounded">
+          <input type="search"
+            className="form-control rounded"
+            placeholder="Search"
+            aria-label="Search"
+            aria-describedby="search-addon"
+          />
+          <span className="input-group-text border-0" id="search-addon">
+            <i className="fas fa-search" />
+          </span>
+        </div>
         <div className="row">
           <div className="col-sm-6">
             <div className="card">
               <div className="card-body">
                 <h5 className="card-title">Organizaiton Name</h5>
                 <h6 id="location">Location</h6>
-                <p className="card-text"> Point of Contact Email</p>
-                {/* <NavLink className="green-btn" to="/SignUp/mentor">Email</NavLink> */}
+                <p className="card-text"> Point of Contact Name</p>
+                <p className="card-text"> Website</p>
+                {/* // add on click to make it functional */}
                 <i className="far fa-envelope green-btn" />
               </div>
             </div>
@@ -35,8 +42,8 @@ class NetworkMentor extends Component {
               <div className="card-body">
                 <h5 className="card-title">Organizaiton Name</h5>
                 <h6 id="location">Location</h6>
-                <p className="card-text"> Point of Contact Email</p>
-                {/* <NavLink className="green-btn" to="/SignUp/mentor">Email</NavLink> */}
+                <p className="card-text"> Point of Contact Name</p>
+                <p className="card-text"> Website</p>
                 <i className="far fa-envelope green-btn" />
               </div>
             </div>
