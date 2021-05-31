8157b02e14878fb7003bf05d384bfb0a8b36d8a7
willperez23 <william.perez.23@dartmouth.edu>
2021-05-31 18:19:51 -0400

    basic game of life working

--- src/components/YouthOnboarding/onBoardingCard.js
+++ src/components/YouthOnboarding/onBoardingCard.js
@@ -1,67 +1,86 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 // import { connect } from 'react-redux';
 // DONT FORGET NAV LINK
 // import { withRouter } from 'react-router-dom';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// DONT FORGET NAV LINK
+import { withRouter } from 'react-router-dom';
+import './onBoardingCard.scss';
+// import OnBoardingCards from './onBoardingCard';
 
-const OnBoardingCards = ({ title, hoverText, transitionTitleText }) => {
-  const [hover, setHover] = useState(false);
-  const [transition, setTransition] = useState(false);
+// const OnBoardingCards = ({ title, hoverText, transitionTitleText, onClick }) => {
+//   const [hover, setHover] = useState(false);
+//   const [transition, setTransition] = useState(false);
 
-  console.log(transitionTitleText);
+//   console.log(transitionTitleText);
 
-  const handleHover = () => {
-    setHover(!hover);
-  };
+//   const handleHover = () => {
+//     setHover(!hover);
+//   };
 
-  const handleClick = () => {
-    setTransition(!transition);
-  };
+//   const handleClick = () => {
+//     setTransition(!transition);
+//   };
 
-  return (
-    <div>
-      { transition ? <p> {transitionTitleText}</p> : (
-        <div>
-          <div className="onBoardingCard" onClick={handleClick} role="button" tabIndex="0">
-            <h1 className="title" onMouseOver={handleHover} onFocus={handleHover}>{title}</h1>
-          </div>
-          <div>
-            {hover ? <p>{hoverText}</p> : null}
-          </div>
-        </div>
-      ) }
-    </div>
-
-  );
-};
-
-// class OnBoardingCards extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       text: this.props.forCard.title,
-//       hover: false,
-//       hoverText: this.props.forCard.hoverText,
-
-//     };
-//   }
-
-//   handleHover = () => {
-//     this.setState({ hover: !this.hover });
-//   }
-
-//   render = () => {
-//     console.log(this.props.forCard.title, this.props.forCard.hoverText);
-//     return (
-//       <div>
-//         <div className="onBoardingCard">
-//           <h1 className="title" onMouseOver={this.handleHover} onFocus={this.handleHover}>{this.state.text}</h1>
-//         </div>
-//         <div>
-//           {this.state.hover ? <p>{this.state.hoverText}</p> : null}
-//         </div>
+//   return (
+//     <div>
+//       {/* { transition ? <p> {transitionTitleText}</p> : ( */}
+//       {/* <div> */}
+//       <div className="onBoardingCard" onClick={this.PaymentResponse.} role="button" tabIndex="0">
+//         <h1 className="title" onMouseOver={handleHover} onFocus={handleHover}>{title}</h1>
 //       </div>
-//     );
-//   }
-// }
+//       <div>
+//         {hover ? <p>{hoverText}</p> : null}
+//       </div>
+//       {/* </div> */}
+//       {/* ) } */}
+//     </div>
 
-export default OnBoardingCards;
+//   );
+// };
+
+class OnBoardingCards extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: this.props.title,
+      hover: false,
+      hoverText: this.props.hoverText,
+      transitionTitleText: this.props.transitionTitleText,
+      titleText: this.props.titleText,
+      options: this.props.option,
+    };
+  }
+
+  handleHover = () => {
+    this.setState({ hover: !this.hover });
+  }
+
+  handleClick = () => {
+    this.props.setTransitionText(this.state.transitionTitleText);
+    this.props.setTitleText(this.state.titleText);
+    // console.log('card console log options: ', this.props.options);
+    this.props.setOptions(this.state.options);
+    console.log('conclusionText: ', this.props.conclusionText);
+    if (this.props.conclusionText !== null) {
+      console.log('inconclusionText');
+      this.props.setConclusionText(this.props.conclusionText);
+    }
+  }
+
+  render = () => {
+    console.log(this.props.title, this.props.hoverText);
+    return (
+      <div>
+        <div className="onBoardingCard" onClick={this.handleClick} role="button" tabIndex="0">
+          <h1 className="title" onMouseEnter={this.handleHover} onMouseLeave={this.handleHover} onFocus={this.handleHover}>{this.state.text}</h1>
+        </div>
+        <div>
+          {this.state.hover ? <div className="onBoardingHover">{this.state.hoverText}</div> : null}
+        </div>
+      </div>
+    );
+  }
+}
+export default withRouter(connect(null, {})(OnBoardingCards));
--- src/components/YouthOnboarding/onBoardingPage.js
+++ src/components/YouthOnboarding/onBoardingPage.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-// DONT FORGET NAV LINK
-import { withRouter } from 'react-router-dom';
+
+import { withRouter, NavLink } from 'react-router-dom';
 import OnBoardingCards from './onBoardingCard';
+import './onBoardingPage.scss';
 
 const jsonFile = require('../../constants/test.json');
 
@@ -10,7 +11,12 @@ class OnBoardingPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      titleText: jsonFile[0].titleText,
+      transitionTitleText: '',
+      transition: false,
+      options: jsonFile[0].options,
+      // first: true,
+      conclusionText: '',
     };
   }
 
@@ -39,6 +45,29 @@ class OnBoardingPage extends Component {
   //   array.forEach()
   // }
 
+  setConclusionText = (text) => {
+    console.log('made it in set conclusion text');
+    this.setState({ conclusionText: text });
+  }
+
+  setTransitionText = (text) => {
+    this.setState({ transitionTitleText: text });
+    this.setState({ transition: true });
+  }
+
+  setTitleText = (text) => {
+    this.setState({ titleText: text });
+  }
+
+  setOptions = (optionsArray) => {
+    this.setState({ options: optionsArray });
+  }
+
+  continueClick = () => {
+    this.setState({ transition: false });
+    // this.setState({ first: false });
+  }
+
   mapJson = (fileArray) => {
     console.log('json file:', fileArray);
     return fileArray.map((data) => {
@@ -54,10 +83,43 @@ class OnBoardingPage extends Component {
                 title={value.title}
                 hoverText={value.hoverText}
                 transitionTitleText={value.transitionTitleText}
+                setTransitionText={this.setTransitionText}
+                titleText={value.titleText}
+                setTitleText={this.setTitleText}
+                option={data.options}
+                setOptions={this.setOptions}
+                conclusionText={data.conclusionText}
+                setConclusionText={this.setConclusionText}
               />
             </div>
           );
         }));
+    });
+  }
+
+  mapJsonAfter = (fileArray) => {
+    console.log('json file:', fileArray);
+    return fileArray.map((data) => {
+      console.log('data from mapJsonAfter', data);
+      console.log('array from after .map: ', data.options);
+      return (
+      // need a way to iterate through all of the things in data and use the json object that is returned to populate onBoardingCard component
+        <div key={data.id} className="accordion" id="accordionPanelsStayOpenExample">
+          <OnBoardingCards
+            key={data.id}
+            title={data.title}
+            hoverText={data.hoverText}
+            transitionTitleText={data.transitionTitleText}
+            setTransitionText={this.setTransitionText}
+            titleText={data.titleText}
+            setTitleText={this.setTitleText}
+            option={data.options}
+            setOptions={this.setOptions}
+            conclusionText={data.conclusionText}
+            setConclusionText={this.setConclusionText}
+          />
+        </div>
+      );
     });
   }
 
@@ -67,26 +129,33 @@ class OnBoardingPage extends Component {
   // }
 
   render = () => {
-    return (
+    if (!this.state.transition) {
+      return (
 
-      <div className="onBoardingPage">
-        <h1 className="title">{jsonFile[0].titleText}</h1>
-        {this.mapJson(jsonFile)}
-        {/* {jsonFile.map((data, key) => {
-          return (
-            // eslint-disable-next-line react/no-array-index-key
-            <div key={key}>
-              <div>{data.options.map((data, key) => {
-
-              })}
-              </div>
-              <div>{key}</div>
+        <div className="onBoardingPage">
+          <h1 className="title onBoardingTitle">{this.state.titleText}</h1>
+          <div className="onBoardingCards">{this.mapJsonAfter(this.state.options)} </div>
+          {/* {this.state.conclusionText !== '' ? <h1 className="title">{this.state.conclusionText}</h1> : <h1 className="title">{this.state.titleText}</h1> } */}
+          {/* {this.mapJsonAfter(this.state.options)} */}
+          {/* { this.state.first ? <div className="onBoardingCards">{this.mapJson(this.state.options)}</div> : (
+            <div className="onBoardingCards">{this.mapJsonAfter(this.state.options)}
             </div>
-          );
-        })} */}
+          )} */}
+        </div>
+      );
+    } else {
+      return (
+        <div className="onBoardingPage">
+          {/* <h1>{this.state.transitionTitleText}</h1> */}
+          {console.log('conslusionText is : ', this.state.conclusionText)}
+          {this.state.conclusionText !== undefined ? <h1 className="title onBoardingTitle">{this.state.conclusionText}</h1>
+            : <h1 className="title onBoardingTitle">{this.state.transitionTitleText}</h1>}
 
-      </div>
-    );
+          {this.state.conclusionText !== undefined ? <NavLink type="button" to="/signup/youth">Finalize Account</NavLink> : <button type="button" onClick={this.continueClick}>Continue </button>}
+        </div>
+
+      );
+    }
   }
 }
 
