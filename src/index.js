5c8bc32c6d3c86dcee16d4208ee03ca3dbf10a2f
Melissa Valencia <melissa.valencia.23@dartmouth.edu>
2021-06-07 16:57:51 -0700

    done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
-// change require to es6 import style
-// import $ from 'jquery';
-// import './style.scss';
-
-// let num = 0;
-
-// setInterval(() => { $('#main').html(`Youve been on this page for ${num} seconds.`); num += 1; }, 1000);
-// // const $ = require('jquery');
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
